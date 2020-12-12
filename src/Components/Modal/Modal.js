e8f2936b4aea477db5349886d91754a4e67b6f93
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-12 22:52:42 +0200

    Update Modal.js

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleEscKeydown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleEscKeydown);
+  }
+
+  handleEscKeydown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={s.overlay} onClick={this.handleBackdropClick}>
+        <div className={s.modal}>
+          <img src="" alt="" />
+        </div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
