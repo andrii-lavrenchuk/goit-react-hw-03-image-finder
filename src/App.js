53320d86a84d1ce5aceaba0ac2c4b3745d821121
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-26 19:51:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
 import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
 // import shortid from 'shortid';
 // import Form from './Components/Form/Form';
 // import ContactList from './Components/ContactList/ContactList';
 // import Filter from './Components/Filter/Filter';
 // import Title from './Components/Title/Title';
 // import Container from './Components/Container/Container';
-import Searchbar from './Components/Searchbar/Searchbar';
-import Modal from './Components/Modal/Modal';
+import ImageGallery from './Components/ImageGallery/ImageGallery';
+// import ImagesInfo from './Components/ImagesInfo/ImagesInfo';
+// import Modal from './Components/Modal/Modal';
+// import Searchbar from './Components/Searchbar/Searchbar';
 
 class App extends Component {
+  // state = {
+  //   imageName: '',
+  // };
   state = {
-    showModal: false,
-  };
+    // searchQuery: '',
+    // showModal: false,
 
+    images: [],
+    currentPage: 1,
+    searchQuery: '',
+    // isLoading: false,
+    error: null,
+  };
   toggleModal = () => {
     this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
 
+  // handleFormSubmit = imageName => {
+  //   this.setState({ imageName });
+  // };
+
+  // onChangeQuery = query => {
+  //   this.setState({
+  //     searchQuery: query,
+  //     currentPage: 1,
+  //     images: [],
+  //     error: null,
+  //   });
+  // };
+
   render() {
-    const { showModal } = this.state;
+    // const { showModal } = this.state;
 
     return (
       <div>
+        {/* <Searchbar onSubmit={this.onChangeQuery} /> */}
+
+        <ToastContainer autoClose={3000} />
         {/* <button type="button" onClick={this.toggleModal}>
           Open
         </button> */}
-        {showModal && <Modal onClose={this.toggleModal}></Modal>}
-        <Searchbar />
+        {/* <Searchbar onSubmit={this.handleFormSubmit} /> */}
+        {/* <ImageGallery imageName={this.state.imageName} /> */}
+
+        {/* {showModal && <Modal onClose={this.toggleModal}></Modal>} */}
+
+        <ImageGallery />
       </div>
     );
   }
